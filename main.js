5c1e66983a5cd0c16e08e919c3da958e41a53324
Vitor Lima <92996466+VitorFSLima@users.noreply.github.com>
2021-10-22 14:09:45 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const linksSocialMedia = {
+  linkedin: 'in/vitor-lima-a27146190',
+  github: 'VitorFSLima',
+  facebook: 'vitor.ferreira.311',
+  instagram: 'vitorfslima',
+  twitter: 'jakelinytec'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name // para mudar texto
+      userBio.textContent = data.bio
+      userLink.href = data.html_url // para mudar link
+      userImage.src = data.avatar_url // para mudar imagem
+      userLogin.textContent = data.login
+    })
+
+  //alert(url)
+}
+
+getGitHubProfileInfos()
